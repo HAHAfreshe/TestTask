617974c2742f53b491165977452270ad14386290
Nikitenko <nikitenko.ka@ubmail.ru>
2021-06-22 11:07:19 +0600

    Work is done

--- TestTask.c
+++ TestTask.c
@@ -12,7 +12,7 @@ void replaceAll(char *str, const char *oldWord, const char *newWord, char *path,
 int main(int argc, char *argv[])
 {
     if (argc == 1){
-        printf("syntax : \t%s --help \n", argv[0]);
+        printf("Syntax : \t%s --help \n", argv[0]);
         return 0;
     }
 
@@ -27,14 +27,16 @@ int main(int argc, char *argv[])
     FILE * fTemp;
     FILE * fLog;
     char path[strlen(argv[1])+1];
-    char pathToFile[1000];
+    char pathToFile[BUFFER_SIZE];
     char logFileName[16];
     time_t t = time(NULL);
     struct tm* aTm = localtime(&t);
-    int i = 1;
+    int numbStr;
 
     snprintf(path, sizeof path, "%s", argv[1]);
-    snprintf(logFileName, sizeof logFileName, "%04d%02d%02d-%02d%02d%02d%s", aTm->tm_year+1900, aTm->tm_mon+1, aTm->tm_mday, aTm->tm_hour, aTm->tm_min, aTm->tm_sec, ".log");
+    snprintf(logFileName, sizeof logFileName, "%04d%02d%02d-%02d%02d%02d", aTm->tm_year+1900, aTm->tm_mon+1, aTm->tm_mday, aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
+    strcat(logFileName, ".log");
+
     char buffer[BUFFER_SIZE];
     struct dirent *dir;
 
@@ -46,16 +48,14 @@ int main(int argc, char *argv[])
     fLog = fopen(logFileName, "a");
 
     if (d) {
-        
         while ((dir = readdir(d)) != NULL) {
             snprintf(pathToFile, sizeof pathToFile, "%s/%s", path, dir->d_name);
             fPtr  = fopen(pathToFile, "r");
-            if (fPtr)
-            {   i = 0;
+            if (fPtr){   numbStr = 0;
                 fTemp = fopen("replace.tmp", "w"); 
                 while ((fgets(buffer, BUFFER_SIZE, fPtr)) != NULL)
-                {   i++;
-                    replaceAll(buffer, argv[2], argv[3], pathToFile, fLog, i);
+                {   numbStr++;
+                    replaceAll(buffer, argv[2], argv[3], pathToFile, fLog, numbStr);
                     fputs(buffer, fTemp);
                 }
                 fclose(fPtr);
@@ -64,21 +64,42 @@ int main(int argc, char *argv[])
                 rename("replace.tmp", pathToFile);
                 remove("replace.tmp");
             }
-            else
-            {
+            else{
                 printf("\nUnable to open file: %s.\n", pathToFile);
                 printf("Please check whether file exists and you have read/write privilege.\n");
+
+                fprintf(fLog, "\tUnable to open file: %s.\n", pathToFile);
+                fprintf(fLog, "\tPlease check whether file exists and you have read/write privilege.");
             }
         }
         closedir(d);
     }
+    else{
+        printf("\nUnable to find directory: %s.\n", path);
+        printf("Please check whether directory exists and you have read/write privilege.\n");
+
+        fprintf(fLog, "\tUnable to find directory: %s.\n", path);
+        fprintf(fLog, "\tPlease check whether directory exists and you have read/write privilege.");
+    }
+
     fclose(fLog);
 
     return 0;
 }
 
-
-void replaceAll(char *str, const char *oldWord, const char *newWord, char *path, FILE *fLog, int i)
+/*
+Funct "replaceAll" - finding a string with search character,
+replacing search characters with new ones
+and write to the log file
+params:
+    char *str - full string from file
+    const char *oldWord - searching word
+    const char *newWord - The word to replace it
+    char *path - path to file (for log-file)
+    FILE *fLog - log - file
+    int numbStr - number of the line
+*/
+void replaceAll(char *str, const char *oldWord, const char *newWord, char *path, FILE *fLog, int numbStr)
 {
     char *pos, temp[BUFFER_SIZE];
     int index = 0;
@@ -98,7 +119,7 @@ void replaceAll(char *str, const char *oldWord, const char *newWord, char *path,
 
         strcat(str, newWord);
         strcat(str, temp + index + owlen);
-        
-        fprintf(fLog, "\t%s: %d:%d: \"%s\" -> \"%s\"\n", path, i, pos-str+1, oldWord, newWord);
+
+        fprintf(fLog, "\t%s: %d:%d: \"%s\" -> \"%s\"\n", path, numbStr, index+1, oldWord, newWord);
     }
 }
